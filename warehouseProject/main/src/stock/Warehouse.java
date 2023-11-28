4abe9bfe0ff3caf0b6b09abfdf7635e20d26ec57
dci-student <youssef.elkhoury@dci-student.org>
2023-11-28 09:16:33 +0100

    In stock package I created 2 java classes 1.item and 2.Warehouse here in those 2 classes will create the fields of every class according to the needs and constructor's, getter and setter, and to string method,and in util package I created a class called applyToAllMethods.java here I will  implement all methods that will apply to all classes.

--- warehouseProject/main/src/stock/Warehouse.java
+++ warehouseProject/main/src/stock/Warehouse.java
@@ -1,38 +1,4 @@
 package stock;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Warehouse {
-    private int id;
-    private List<Item> items;
-
-    public Warehouse(int id) {
-        this.id = id;q
-        this.items = new ArrayList<>();
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void addItem(Item item) {
-        items.add(item);
-    }
-
-    public List<Item> getItems() {
-        return items;
-    }
-
-    public int occupancy() {
-        return items.size();
-    }
-
-    public Item search(String name) {
-        for (Item item : items) {
-            if (item.getName().equalsIgnoreCase(name)) {
-                return item;
-            }
-        }
-        return null;
-    }
 }
